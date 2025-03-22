4f9aa8bcd309072622aeeea05172b32832e94e3b
Thiaguitosz <tam7@aluno.ifnmg.edu.br>
2025-03-22 01:46:18 +0000

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express(); // Instancia o Express
 const port = 3000; // Define a porta
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
   console.log("Rota / solicitada");
